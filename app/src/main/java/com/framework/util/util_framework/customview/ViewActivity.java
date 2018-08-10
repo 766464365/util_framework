3020f4627075f9f2f19385cb784dcbd050a943d0
john <766464365@qq.com>
2018-08-10 15:41:30 +0800

    new switchButton

---
+++ app/src/main/java/com/framework/util/util_framework/customview/ViewActivity.java
@@ -0,0 +1,30 @@
+package com.framework.util.util_framework.customview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.framework.util.util_framework.R;
+import com.xuwei.framework.customview.SlideSwitchButton;
+
+public class ViewActivity extends AppCompatActivity implements SlideSwitchButton.SlideListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+        SlideSwitchButton slideSwitchButton1 = (SlideSwitchButton) findViewById(R.id.slide2);
+        slideSwitchButton1.setSlideListener(this);
+        slideSwitchButton1.changeOpenState(false);
+    }
+
+    @Override
+    public void openState(boolean isOpen, View view) {
+        switch (view.getId()){
+            case R.id.slide2:
+                Toast.makeText(this,"状态"+isOpen,Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
